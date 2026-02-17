19afcf483cadd4a368c51b17d00b70b288376fae
LiZhongYuan <li.zhong.yuan@outlook.com>
2026-02-18 01:45:33 +0800

    add sth

--- src/test/sequential_operation/test_add.py
+++ src/test/sequential_operation/test_add.py
@@ -11,14 +11,16 @@ from test.instance import atomOie_1, atomOie_2
 
 
 def test_addition_with_identical_operands():
+    # ---------- 1 Init an OIES instance ----------
     oieS = OIES(atomOie_1, atomOie_1)
-
+    # ---------- 2 Set infeasible 2TupleTS instance to the OIES instance ----------
+    # 2.1  Init an infeasible 2TupleTS instance, empty set
     infeasible_2tupleTS = TwoTupleTS()
+    # 2.2 Assign to the member of OIES instance
     oieS.set_wildcard_infeasible_2tupleTS(infeasible_2tupleTS, (atomOie_1, atomOie_1))
-
+    # ---------- 3 Complete Sequential Addition ----------
     domain_filtering_2tuple = (1, 5)
     oie_res = complete_sequential_addition(oieS, (1, 2), domain_filtering_2tuple)
-
     print(f"oie_res: {str(oie_res)}\n")
 
 
