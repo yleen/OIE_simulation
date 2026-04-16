cdd933e804e2e4af5dbfad21e3cc32be17430601
Allen <yuleen_@outlook.com>
2026-04-16 22:35:36 +0800

    set diagram func

--- src/main.py
+++ src/main.py
@@ -7,9 +7,12 @@
 
 from test.sequential_operation.test_add import test_addition_valid_1
 from test.sequential_operation.test_multi import test_multiplication_valid_1
+from LiZhongYuan_diagram.cayley_table.main import run as run_cayley_table
 
 if __name__ == '__main__':
 
     test_addition_valid_1()
 
     test_multiplication_valid_1()
+
+    run_cayley_table()
